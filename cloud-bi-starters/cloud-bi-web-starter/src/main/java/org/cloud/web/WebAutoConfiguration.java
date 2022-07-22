eb089f9881a936e16624d0605947138fb32fb472
Tubetrue01 <Tubetrue01@gmail.com>
2022-07-22 17:15:43 +0800

    准备 web starter 组件；

---
+++ cloud-bi-starters/cloud-bi-web-starter/src/main/java/org/cloud/web/WebAutoConfiguration.java
@@ -0,0 +1,13 @@
+package org.cloud.web;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 描述：开启自动配置
+ *
+ * @author Tubetrue01@gmail.com 2022/7/22
+ */
+@Configuration(proxyBeanMethods = false)
+public class WebAutoConfiguration {
+}
